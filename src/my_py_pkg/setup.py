8cf1dd65d1722bc6b09f6fd55adc62e8a2fdb834
vuluongd <vul45845@gmail.com>
2026-02-24 17:47:15 +0700

    first commit

---
+++ src/my_py_pkg/setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+
+package_name = 'my_py_pkg'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='lunog',
+    maintainer_email='vul45845@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'publisher = my_py_pkg.publisher:main',
+            'subscriber = my_py_pkg.subscriber:main'
+        ],
+    },
+)
