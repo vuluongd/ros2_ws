d4d9188782adc49392de94a992e5d290ed6b92b3
vuluongd <vul45845@gmail.com>
2026-03-02 22:21:20 +0700

    fifth commit

--- src/multi_subscriber_demo/setup.py
+++ src/multi_subscriber_demo/setup.py
@@ -10,6 +10,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', ['launch/multi_demo.launch.py']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -27,8 +28,7 @@ setup(
             'publisher = multi_subscriber_demo.random_publisher_1:main',
             'sub_big = multi_subscriber_demo.subscriber_1:main',
             'sub_even = multi_subscriber_demo.subscriber_2:main',
-            'sub_avg = multi_subscriber_demo.subscriber_3:main'
-
+            'sub_avg = multi_subscriber_demo.subscriber_3:main',
         ],
     },
 )
