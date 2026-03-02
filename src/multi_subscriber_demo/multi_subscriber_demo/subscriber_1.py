181c5977d99c898ed8ddbbf6b3386b9a4c9890b8
vuluongd <vul45845@gmail.com>
2026-03-02 17:46:46 +0700

    xóa

---
+++ src/multi_subscriber_demo/multi_subscriber_demo/subscriber_1.py
@@ -0,0 +1,31 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Int32
+
+class BigSubscriber(Node):
+
+    def __init__(self):
+        super().__init__('big_subscriber')
+
+        self.subscription = self.create_subscription(
+            Int32,
+            'random_number',
+            self.callback,
+            10
+        )
+    
+    def callback(self, msg):
+        if msg.data > 50:
+            self.get_logger().info(f'Big number: {msg.data}')
+
+    
+def main(args=None):
+    rclpy.init(args=args)
+    node = BigSubscriber()
+    rclpy.spin(Node)
+    rclpy.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
+
---
+++ src/multi_subscriber_demo/multi_subscriber_demo/subscriber_2.py
@@ -0,0 +1,31 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Int32
+
+class EvenSubscribers(Node):
+
+    def __init__(self):
+        super().__init__('even_subscribers')
+
+        self.subscription = self.create_subscription(
+            Int32,
+            'random_number',
+            self.callback,
+            10
+        )
+
+    def callback(self, msg):
+        if msg.data %2 == 0:
+            self.get_logger().info(f'Even number: {msg.data}')
+
+def main(args = None):
+    rclpy.init(args = args)
+    node = EvenSubscribers()
+    rclpy.spin(node)
+    rclpy.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
+
+        
