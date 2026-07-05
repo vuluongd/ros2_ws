6dfdf234733d93c8922b1c8d910be024019ed283
vuluongd <vul45845@gmail.com>
2026-07-05 14:53:52 +0700

    subscriber_cpp

--- src/my_cpp_pub_sub/src/publisher.cpp
+++ src/my_cpp_pub_sub/src/publisher.cpp
@@ -13,7 +13,7 @@ public:
         publisher_ = this->create_publisher<std_msgs::msg::String>("chatter", 10);
         timer_ = this->create_wall_timer(
             1s,
-             std::bind(&MinimalPublisher::timer_callback, this);
+            std::bind(&MinimalPublisher::timer_callback, this)
         );
     }
 private:
--- src/my_cpp_pub_sub/src/subscriber.cpp
+++ src/my_cpp_pub_sub/src/subscriber.cpp
@@ -14,12 +14,12 @@ public:
             "chatter",
             10,
             std::bind(&MinimalSubscriber::topic_callback, this, std::placeholders::_1));
-        )
+        
     }
 private:
     void topic_callback(const std_msgs::msg::String::SharedPtr msg)const
     {
-        RCLCPP_INFO(this->get_logger(), "I heard: '%'", msg->data.c_str());
+        RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg->data.c_str());
     }
 
     rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
@@ -30,6 +30,6 @@ int main(int argc, char *argv[])
     rclcpp::init(argc, argv);
     rclcpp::spin(std::make_shared<MinimalSubscriber>());
 
-    rclcpp::shutdown;
+    rclcpp::shutdown();
     return 0;
 }
