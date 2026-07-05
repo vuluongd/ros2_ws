a88c183a7641e12f45f6a1fbcaeeb847b2a5a88c
vuluongd <vul45845@gmail.com>
2026-07-05 17:28:51 +0700

    uav_state_estimator

--- src/uav_state_estimator/include/uav_state_estimator/ekf_core.hpp
+++ src/uav_state_estimator/include/uav_state_estimator/ekf_core.hpp
@@ -13,6 +13,39 @@ public:
     ~EKFcore() = default;
 
     void setInitialState(const Eigen::VectorXd& x0, const Eigen::MatrixXd& P0);
-    
+    void predict(double dt, const Eigen::Vector3d& gyro, const Eigen::Vector3d& acc);
+    void updateGPS(const Eigen::Vector3d& pos, const Eigen::Vector3d& vel = Eigen::Vector3d::Zero());
+
+    Eigen::VectorXd getState const  {return x_;}
+    Eigen::MatrixXd getCovariance const {return P_;}
+
+    // Get position, velocity, attitude (quaternion)
+
+    Eigen::Vector3d getPosition() const { return x_.segment<3>(0); }
+    Eigen::Vector3d getVelocity() const { return x_.segment<3>(3); }
+    Eigen::Quaterniond getAttitude() const {
+        Eigen::Quaterniond q(x_(6), x_(7), x_(8), x_(9));
+        q.normalize();
+        return q;
+    }
+private:
+     // State and covariance
+    Eigen::VectorXd x_; //16*1
+    Eigen::MatrixXd P_;  //16*16
+
+     // Noise covariance matrices
+     Eigen::MatrixXd Q_;  
+     Eigen::MatrixXd R_;
+
+     // Helper functions
+     Eigen::MatrixXd computeStateTransitionJacobian(double dt);
+     Eigen::MatrixXd computeProcessNoiseCovariance(double dt);
+     Eigen::MatrixXd computeMeasurementJacobian(double dt);
+     Eigen::VectorXd computeMeasurementPrediction();
+
+     Eigen::Quaterniond quaternionFromVec(const Eigen::Vector3d& v);
+     Eigen::Vector3d quaternionRotate(const Eigen::Quaterniond& q, const Eigen::Vector3d& v);
+};
+
 }
-}
+#endif  // EKF_CORE_HPP_
