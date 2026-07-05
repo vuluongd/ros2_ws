5eb4aeb7d36b2bfdb4050309f9bd13d1495dd8f5
vuluongd <vul45845@gmail.com>
2026-07-05 21:37:14 +0700

    uav_state_estimator

--- src/uav_state_estimator/src/ekf_core.cpp
+++ src/uav_state_estimator/src/ekf_core.cpp
@@ -18,7 +18,11 @@ EkfCore::EkfCore(){
      // Process noise (IMU)
 
      Q_ = Eigen::MatrixXd::Zero(12,12);
+     Q_.block<3,3>(0,0) = Eigen::Matrix3d::Identity() * 0.01; 
+     Q_.block<3,3>(3,3) = Eigen::Matrix3d::Identity() * 0.01;
+     Q_.block<3,3>(6,6) = Eigen::Matrix3d::Identity() * 0.01;
      
+
 
 
 }
