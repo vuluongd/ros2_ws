3fe25f024dbfdb968122eed59902f250e2a6ab70
vuluongd <vul45845@gmail.com>
2026-07-05 21:33:18 +0700

    uav_state_estimator

--- src/uav_state_estimator/src/ekf_core.cpp
+++ src/uav_state_estimator/src/ekf_core.cpp
@@ -2,5 +2,25 @@
 #include <iostream>
 
 namespace uav_state_estimator {
-EkfCore::EkfCore()
+EkfCore::EkfCore(){
+    x_ = Eigen::VectorXd::Zero(16);
+    x_(6) = 1.0;
+
+    // Covariance: large initial uncertainty for position/velocity, small for biases
+
+    P_ = Eigen::MatrixXd::zero(12,12);
+    P_.block<3,3>(0,0) *= 10.0; // position
+    P_.block<3,3>(3,3) *= 10.0; // velocity
+    P_.block<3,3>(6,6) *= 0.01; // attitude (small)
+    P_.block<3,3>(10,10) *= 0.1; // accel bias
+    P_.block<3,3>(13,13) *= 0.1; //gyro bias
+
+     // Process noise (IMU)
+
+     Q_ = Eigen::MatrixXd::Zero(12,12);
+     
+
+
+}
+
 }
